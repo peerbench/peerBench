c1eba5e624deac5cfa0e1963047420ac48a98c3d
Muhammed Karamuk <mkaramuk@proton.me>
2025-12-01 23:57:13 +0300

    feat(webapp): allow passing `null` state to `InferData`

--- apps/webapp/src/database/utilities.ts
+++ apps/webapp/src/database/utilities.ts
@@ -27,23 +27,27 @@ export type InferColumn<
  * Also supports inferring an object "shape" from a `ColumnsSelection`.
  * @link https://github.com/emmnull/drizzle-orm-helpers
  */
-export type InferData<T extends SQLWrapper | ColumnsSelection> =
-  T extends ColumnsSelection
-    ? {
-        [K in keyof T]: T[K] extends SQLWrapper | ColumnsSelection
-          ? InferData<T[K]>
-          : T[K];
-      }
-    : T extends Table
-      ? InferSelectModel<T>
-      : T extends Column
-        ? T["_"]["notNull"] extends true
+export type InferData<
+  T extends SQLWrapper | ColumnsSelection,
+  IsNonNullable extends boolean = false,
+> = T extends ColumnsSelection
+  ? {
+      [K in keyof T]: T[K] extends SQLWrapper | ColumnsSelection
+        ? InferData<T[K]>
+        : T[K];
+    }
+  : T extends Table
+    ? InferSelectModel<T>
+    : T extends Column
+      ? IsNonNullable extends true
+        ? T["_"]["data"]
+        : T["_"]["notNull"] extends true
           ? T["_"]["data"]
           : T["_"]["data"] | null
-        : T extends View | Subquery
-          ? T["_"]["selectedFields"]
-          : T extends SQL<infer U>
+      : T extends View | Subquery
+        ? T["_"]["selectedFields"]
+        : T extends SQL<infer U>
+          ? U
+          : T extends SQL.Aliased<infer U>
             ? U
-            : T extends SQL.Aliased<infer U>
-              ? U
-              : unknown;
+            : unknown;
