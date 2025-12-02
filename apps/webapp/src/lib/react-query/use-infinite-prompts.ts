c2a8e360f8b56a0164e88a62c806c23b5193ac09
Muhammed Karamuk <mkaramuk@proton.me>
2025-12-02 19:50:27 +0300

    fix(webapp): wrong client cache manipulation

--- apps/webapp/src/lib/react-query/use-infinite-prompts.ts
+++ apps/webapp/src/lib/react-query/use-infinite-prompts.ts
@@ -2,21 +2,27 @@ import { RequestQueryParams as GetPromptsRequestQueryParams } from "@/app/api/v2
 import { QK_PROMPTS } from "./query-keys";
 import { useInfiniteQuery, UseInfiniteQueryParams } from "./use-infinite-query";
 import { APIPromptItem, usePromptAPI } from "../hooks/use-prompt-api";
+import { useMemo } from "react";
 
 export function useInfinitePrompts(
   params: GetPromptsRequestQueryParams = {},
   infiniteQueryParams?: Partial<UseInfiniteQueryParams<APIPromptItem>>
 ) {
   const { getPrompts } = usePromptAPI();
-  return useInfiniteQuery({
-    queryKey: [`${QK_PROMPTS}/infinite`, params],
-    queryFn: ({ pageParam }) =>
-      getPrompts({
-        page: pageParam,
-        pageSize: params.pageSize ?? 10,
-        ...params,
-      }),
-    initialPageParam: 1,
-    ...(infiniteQueryParams || {}),
-  });
+  const queryKey = useMemo(() => [`${QK_PROMPTS}/infinite`, params], [params]);
+
+  return [
+    useInfiniteQuery({
+      queryKey,
+      queryFn: ({ pageParam }) =>
+        getPrompts({
+          page: pageParam,
+          pageSize: params.pageSize ?? 10,
+          ...params,
+        }),
+      initialPageParam: 1,
+      ...(infiniteQueryParams || {}),
+    }),
+    queryKey,
+  ] as const;
 }
