6c49fa973a855df636fde0d3be46fab839958c58
Muhammed Karamuk <mkaramuk@proton.me>
2025-12-19 14:55:49 +0300

    build(webapp): use mounted .env file

--- apps/webapp/scripts/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# Copy .env file to the working directory if it is mounted to the secrets folder
-if [ -f /secrets/.env ]; then
-  cp /secrets/.env ./apps/webapp/.env
-fi
-
-# Start the server
-exec node apps/webapp/server.js
